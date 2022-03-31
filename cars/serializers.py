8ce8da9bf66b2fc2779f8608a21ecbc0ab8b9c1d
Dyshine Hill <eyedolarts@gmail.com>
2022-03-31 14:33:27 -0500

    updated ability to create update  and get cars from our database

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializer
+from rest_framework import serializers
 from .models import Car
 
 
 
-class CarSerializer(serializer.ModelSerializer):
+class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id','make','model','year','price']
